1e0ea05327065d55e7d17a163c4c2aa79259d185
lucastimho <lucas.t.ho.lh@gmail.com>
2022-01-27 12:00:16 -0600

    adds Bel-Air lyrics

--- function.js
+++ function.js
@@ -4,3 +4,11 @@ for (let i = 0; i < tempF.length; i++) {
   tempC.push(tempF[i] - 32 * 0.5556);
 }
 console.log(tempC);
+
+function printLyrics() {
+  console.log("Now this is a story all about how");
+  console.log("My life got flipped turned upside down");
+  console.log("And I'd like to take a minute, just sit right there");
+  console.log("I'll tell you how I became the prince of a town called Bel-Air");
+}
+printLyrics();

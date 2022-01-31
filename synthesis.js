876f9cc74dd75ff805e777c1c170805a74f16f79
lucastimho <lucas.t.ho.lh@gmail.com>
2022-01-31 14:45:56 -0600

    adds letters

--- synthesis.js
+++ synthesis.js
@@ -3,6 +3,11 @@ for (let i = 10; i > 0; i--) {
 }
 console.log("BLASTOFF!");
 
-// for (let i = 1; i < 10000; i *= 2) {
-//   console.log(i);
-// }
+for (let i = 1; i < 10000; i *= 2) {
+  console.log(i);
+}
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+for (let i = 0; i < letters.length; i += 2) {
+  console.log(letters[i]);
+}

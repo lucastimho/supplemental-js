687a46a6b2daf5ea2086ce9a781e5da2f824bc35
lucastimho <lucas.t.ho.lh@gmail.com>
2022-02-02 18:35:54 -0600

    adds converter function

--- synthesis.js
+++ synthesis.js
@@ -22,3 +22,41 @@ var letter = [
 letter.forEach(function (a) {
   console.log(a[1]);
 });
+
+function code(a) {
+  var converter = {
+    A: "Z",
+    B: "Y",
+    C: "X",
+    D: "W",
+    E: "V",
+    F: "U",
+    G: "T",
+    H: "S",
+    I: "R",
+    J: "Q",
+    K: "P",
+    L: "O",
+    M: "N",
+    N: "M",
+    O: "L",
+    P: "K",
+    Q: "J",
+    R: "I",
+    S: "H",
+    T: "G",
+    U: "F",
+    V: "E",
+    W: "D",
+    X: "C",
+    Y: "B",
+    Z: "A",
+  };
+  var b = a.split("");
+  var d = [];
+  for (let i = 0; i < b.length; i++) {
+    d.push(converter[b[i].toUpperCase()]);
+  }
+  return d.join("");
+}
+console.log(code("hello"));
